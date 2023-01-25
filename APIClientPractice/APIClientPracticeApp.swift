c1bded82ecbe7f39fca75a21c7f4225449c14f5c
jun.morita <jun.morita@gree.net>
2023-01-25 10:25:49 +0900

    Initial Commit

---
+++ APIClientPractice/APIClientPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIClientPracticeApp.swift
+//  APIClientPractice
+//
+//  Created by Jun Morita on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct APIClientPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
